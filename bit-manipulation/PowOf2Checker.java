b0763187c5ac913a9775b96e47c89d28725a18c5
AmanMaheshSingh <amanmaheshsingh@gmail.com>
2025-10-25 00:51:05 +0530

    Added New Question to Bit Manipulation

---
+++ bit-manipulation/PowOf2Checker.java
@@ -0,0 +1,15 @@
+//Write a program to find if a number is a power of 2 or not.
+import java.util.*;
+
+class PowOf2Checker{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int n = sc.nextInt();
+        if (n > 0 && (n & (n - 1)) == 0) {
+            System.out.println(n + " is a power of 2");
+        } else {
+            System.out.println(n + " is NOT a power of 2");
+        }
+    }
+}
