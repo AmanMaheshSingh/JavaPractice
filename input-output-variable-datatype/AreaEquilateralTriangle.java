945aa6ae6d0e33fd20d8e5865ca0ce2eb6a9d4ad
AmanMaheshSingh <amanmaheshsingh@gmail.com>
2025-11-03 00:04:59 +0530

    Area Of Equilateral Triangle

---
+++ input-output-variable-datatype/AreaEquilateralTriangle.java
@@ -0,0 +1,12 @@
+//Area Of Equilateral Triangle 0.43
+import java.util.*;
+
+public class AreaEquilateralTriangle{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Side Of Equilateral Triangle");
+        float a = sc.nextFloat();
+        float Area=0.43f*a*a;
+        System.out.println("The Area Of Equilateral Triangle with Given Side : "+a+" is = "+Area);
+    }
+}
