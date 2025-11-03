bed3d53242e4d361ee6dd2a2d198dbb750558a6b
AmanMaheshSingh <amanmaheshsingh@gmail.com>
2025-11-03 17:26:16 +0530

    Perimeter Of Square

---
+++ input-output-variable-datatype/PerimeterSquare.java
@@ -0,0 +1,12 @@
+//Perimeter Of Square
+import java.util.*;
+
+public class PerimeterSquare{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Side Of Square");
+        float a = sc.nextFloat();
+        float Perimeter=4*a;
+        System.out.println("The Perimeter Of Circle with Given Radius : "+a+" is = "+Perimeter);
+    }
+}
