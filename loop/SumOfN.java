c7548216440be9a6fe2d5e9e9c0723e1cae20047
AmanMaheshSingh <amanmaheshsingh@gmail.com>
2025-11-05 16:11:47 +0530

    Sum Of N Numbers

---
+++ loop/SumOfN.java
@@ -0,0 +1,14 @@
+//Sum Of N Numbers
+import java.util.*;
+
+public class SumOfN{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter n");
+        int n = sc.nextInt(),sum= 0;
+        for (int i = 1;i<=n;i++){
+            sum+=i;
+        }
+        System.out.println("Sum of "+n+" numbers is : "+sum);
+    }
+}
